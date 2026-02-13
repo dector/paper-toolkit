520613b032016558403c56cc57f4766ab8111827
D <dector@dector.space>
2026-02-13 01:26:19 +0100

    chore: fix base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: "https://dector.github.io",
-  base: "/",
+  base: "/paper-toolkit",
   integrations: [react()],
 });
